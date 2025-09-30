603bcc4884f0dedcdd07ef17649bc9935847261c
Rain <Valehtelit@mail.ru>
2025-09-30 21:15:55 +1000

    #Функционал по вычислению листков нетрудоспособности

--- src/Application/IntersectionsFinder/Controllers/SickNoteIntersectionsFinderController.php
+++ src/Application/IntersectionsFinder/Controllers/SickNoteIntersectionsFinderController.php
@@ -2,7 +2,20 @@
 
 namespace Application\IntersectionsFinder\Controllers;
 
+use Application\IntersectionsFinder\Models\SickNoteIntersectionsFinder;
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 class SickNoteIntersectionsFinderController
 {
+    public function __construct(private SickNoteIntersectionsFinder $finder){
+
+    }
+
+    public function find(ServerRequestInterface $request) : ResponseInterface{
+        $result = $this->finder->find();
+        return new JsonResponse($result);
+    }
 
 }
--- src/Application/IntersectionsFinder/Models/SickNoteIntersectionsFinder.php
+++ src/Application/IntersectionsFinder/Models/SickNoteIntersectionsFinder.php
@@ -10,12 +10,42 @@ class SickNoteIntersectionsFinder
         $this->pdo = $connector::connect();
     }
 
-    private function findIntersections(){
-        $query = ("");
+    private function dateConvert(array $intersections){
+        $result = [];
+        foreach ($intersections AS $intersection){
+            $intersection['sick_note_patient_date_birth'] = date('d.m.Y', $intersection['sick_note_patient_date_birth']);
+            $intersection['sick_note_open_date'] = date('d.m.Y', $intersection['sick_note_open_date']);
+            if ($intersection['sick_note_closed_date'] === 0){
+                $intersection['sick_note_closed_date'] = 'ЛН открыт';
+            }
+            else{
+                $intersection['sick_note_closed_date'] = date('d.m.Y', $intersection['sick_note_closed_date']);
+            }
+            if ($intersection['medical_history_date_out'] === 0){
+                $intersection['medical_history_date_out'] = 'ИБ открыта';
+            }else{
+                $intersection['medical_history_date_out'] = date('d.m.Y', $intersection['medical_history_date_out']);
+            }
+            $result[] = $intersection;
+        }
+        return $result;
+    }
+
+    private function findHospitalIntersections(){
+        $query = ("SELECT sick_note_unique_id, sick_note_type, sick_note_patient, sick_note_patient_date_birth, 
+                          sick_note_issuing_doctor, sick_note_closed_doctor, sick_note_open_date, sick_note_closed_date, sick_note_days_count, 
+                          sick_note_is_closed, medical_history_date_out
+                   FROM sick_notes
+                   INNER JOIN medical_histories ON medical_history_patient = sick_note_patient");
         $stmt = $this->pdo->prepare($query);
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
+    }
+
+    public function find(){
+        $intersections = $this->findHospitalIntersections();
+        return $this->dateConvert($intersections);
     }
 
 }
