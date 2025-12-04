c47e5005788ca62f129c1b89bfdd47bec2de6f0b
MashMinners <Valehtelit@mail.ru>
2025-12-05 09:52:14 +1000

    #Роут исправлен на более созвучный

--- configs/routes.php
+++ configs/routes.php
@@ -49,4 +49,4 @@ $this->get('invoice/analyzer/incorrect-purpose', '\Application\InvoiceRegisterAn
 $this->get('invoice/analyzer/incorrect-zub', '\Application\InvoiceRegisterAnalyzer\Controllers\StomRegisterAnalyzerController::findIncorrectZub');
 $this->get('invoice/analyzer/incorrect-required-zub', '\Application\InvoiceRegisterAnalyzer\Controllers\StomRegisterAnalyzerController::findRequiredZubCode');
 $this->get('invoice/analyzer/simultaneous-zub-inclusion', '\Application\InvoiceRegisterAnalyzer\Controllers\StomRegisterAnalyzerController::findSimultaneousZubInclusion');
-$this->get('invoice/analyzer/incorrect-usl', '\Application\InvoiceRegisterAnalyzer\Controllers\StomRegisterAnalyzerController::findIncorrectUsl');
+$this->get('invoice/analyzer/incorrect-services', '\Application\InvoiceRegisterAnalyzer\Controllers\StomRegisterAnalyzerController::findIncorrectUsl');
